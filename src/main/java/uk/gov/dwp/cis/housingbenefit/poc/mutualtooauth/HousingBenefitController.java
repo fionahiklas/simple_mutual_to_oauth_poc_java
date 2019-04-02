0276eb9c1ee4660b6ac073156726094a6470b3e9
Fiona Bianchi <fiona@hiklas.co.uk>
2019-04-02 20:03:25 +0100

    Fixed issues with Spring Security getting in the way too much, mutual auth works

--- src/main/java/uk/gov/dwp/cis/housingbenefit/poc/mutualtooauth/HousingBenefitController.java
+++ src/main/java/uk/gov/dwp/cis/housingbenefit/poc/mutualtooauth/HousingBenefitController.java
@@ -3,10 +3,13 @@ package uk.gov.dwp.cis.housingbenefit.poc.mutualtooauth;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 
 @RestController
@@ -14,15 +17,24 @@ public class HousingBenefitController {
 
     private static final Logger log = LogManager.getLogger();
 
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @GetMapping(value = "/get")
-    public String getClaim() {
+    public String getClaim(Principal principal) {
         log.info("getClaim resource called");
+
+        String user = principal.getName();
+        log.info("getClaim principal: {}", user);
+
         return "Get Worked!";
     }
 
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @PostMapping(value = "/post", consumes = MediaType.TEXT_PLAIN_VALUE)
-    public String postClaim(@RequestBody String requestBody) {
+    public String postClaim(@RequestBody String requestBody, Principal principal) {
         log.info("postClaim resource called with request body: {}", requestBody);
+
+        String user = principal.getName();
+        log.info("getClaim principal: {}", user);
         return "Post Worked!";
     }
 }
--- src/main/java/uk/gov/dwp/cis/housingbenefit/poc/mutualtooauth/MainApplication.java
+++ src/main/java/uk/gov/dwp/cis/housingbenefit/poc/mutualtooauth/MainApplication.java
@@ -2,11 +2,44 @@ package uk.gov.dwp.cis.housingbenefit.poc.mutualtooauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(userDetailsService());
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+                return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+
+            }
+        };
     }
 }
 
